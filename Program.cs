52f5204f681f16469a1176411bf46172cf8853c2
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-12 15:33:18 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulationAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();   //Read the Lsit and create the list
+
+            int limit = nums.Count / 2;
+
+            for (int i = 0; i < limit; i++)
+            {
+                nums[i] = nums[i] + nums[nums.Count - 1];
+                nums.RemoveAt(nums.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(' ', nums));
+        }
+    }
+}
